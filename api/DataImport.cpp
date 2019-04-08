fc53e4f562357ac938935c4934b911e46157f20d
bacox <bartcox93@gmail.com>
2019-04-08 14:45:04 +0200

    Add Data import class to import new transactions

---
+++ api/DataImport.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bart on 8-4-19.
+//
+
+#include "DataImport.h"
---
+++ api/DataImport.h
@@ -0,0 +1,16 @@
+//
+// Created by bart on 8-4-19.
+//
+
+#ifndef MONEY_INSIGHT_DATAIMPORT_H
+#define MONEY_INSIGHT_DATAIMPORT_H
+
+
+class DataImport {
+public:
+    void fromCSVFile();
+    void fromStringBuffer();
+};
+
+
+#endif //MONEY_INSIGHT_DATAIMPORT_H
