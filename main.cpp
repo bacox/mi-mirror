d455180bf98964b6c4d4d791c3ea07b8ef935121
bacox <bartcox93@gmail.com>
2019-04-08 15:14:13 +0200

    Add mongoose webserver lib

--- main.cpp
+++ main.cpp
@@ -6,7 +6,27 @@
 #include "api/Transaction.h"
 #include "api/TransactionBook.h"
 #include "api/Database.h"
+#include <mongoose/Server.h>
+#include <mongoose/WebController.h>
 #include <sqlite3.h>
+#include <unistd.h>
+
+
+using namespace Mongoose;
+
+class MyController : public WebController
+{
+public:
+    void hello(Request &request, StreamResponse &response)
+    {
+        response << "Hello " << htmlEntities(request.get("name", "... what's your name ?")) << endl;
+    }
+
+    void setup()
+    {
+        addRoute("GET", "/hello", MyController, hello);
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -31,5 +51,17 @@ int main(int argc, char *argv[])
 //    db.dropTable("TRANSACTIONS");
 
     std::cout << tb << std::endl;
+
+
+    std::cout << "Starting webserver on port 8080" << std::endl;
+    MyController myController;
+    Server server(8080);
+    server.registerController(&myController);
+
+    server.start();
+
+    while (1) {
+        usleep(10);
+    }
     return 0;
 }
